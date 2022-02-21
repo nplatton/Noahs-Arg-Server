8fac0f799df2cbbf402d531db4d351ba311d7de3
Noah Platton <noahplatton@gmail.com>
2022-02-21 13:32:23 +0000

    Update seeding to match source of truth

--- db/seeds.js
+++ db/seeds.js
@@ -9,30 +9,43 @@ db.users.insertMany([
     password_digest:
       "$2a$10$4EAIUWYfI8NC4hSUDyu2R.bGW.zyfKfebOQL6QC6H39VQGK6Reke2",
     org: "Noahs Arg",
-    habits: {
+    tracked_habits: {
       drink_water: {
         target_amount: 4,
         dailyCount: 4,
         weeklyCount: 15,
-        streak: 8,
-        recentStreak: 8,
-        highestStreak: 8,
       },
       break_from_screen: {
         target_amount: 3,
         dailyCount: 2,
         weeklyCount: 10,
-        streak: 2,
-        recentStreak: 3,
-        highestStreak: 3,
       },
       stretch: {
         target_amount: 3,
         dailyCount: 0,
         weeklyCount: 12,
-        streak: 1,
-        recentStreak: 4,
-        highestStreak: 6,
+      },
+    },
+    streaks: {
+      drink_water: {
+        highest: 8,
+        current: 8,
+      },
+      break_from_screen: {
+        highest: 3,
+        current: 2,
+      },
+      stretch: {
+        highest: 6,
+        current: 4,
+      },
+      task4: {
+        highest: 0,
+        current: 0,
+      },
+      task5: {
+        highest: 0,
+        current: 0,
       },
     },
   },
@@ -42,30 +55,43 @@ db.users.insertMany([
     password_digest:
       "$2a$10$cX2K8p9oat9RO5dj2F4VduhN06J6qlH1M6q/v39pFh76kD/SO208q",
     org: "Noahs Arg",
-    habits: {
+    tracked_habits: {
       drink_water: {
         target_amount: 3,
         dailyCount: 2,
         weeklyCount: 10,
-        streak: 2,
-        recentStreak: 3,
-        highestStreak: 3,
       },
       break_from_screen: {
         target_amount: 3,
         dailyCount: 0,
         weeklyCount: 12,
-        streak: 1,
-        recentStreak: 4,
-        highestStreak: 6,
       },
       stretch: {
         target_amount: 4,
         dailyCount: 4,
         weeklyCount: 15,
-        streak: 8,
-        recentStreak: 8,
-        highestStreak: 8,
+      },
+    },
+    streaks: {
+      drink_water: {
+        highest: 3,
+        current: 2,
+      },
+      break_from_screen: {
+        highest: 6,
+        current: 4,
+      },
+      stretch: {
+        highest: 8,
+        current: 8,
+      },
+      task4: {
+        highest: 0,
+        current: 0,
+      },
+      task5: {
+        highest: 0,
+        current: 0,
       },
     },
   },
@@ -75,30 +101,43 @@ db.users.insertMany([
     password_digest:
       "$2a$10$fxXsRktC6JsMsas1RfcQY.uFFg/jAKNPnYjXnYIA/otPJrwkLq0bq",
     org: "Noahs Arg",
-    habits: {
+    tracked_habits: {
       drink_water: {
         target_amount: 3,
         dailyCount: 0,
         weeklyCount: 12,
-        streak: 1,
-        recentStreak: 4,
-        highestStreak: 6,
       },
       break_from_screen: {
         target_amount: 4,
         dailyCount: 4,
         weeklyCount: 15,
-        streak: 8,
-        recentStreak: 8,
-        highestStreak: 8,
       },
       stretch: {
         target_amount: 3,
         dailyCount: 2,
         weeklyCount: 10,
-        streak: 2,
-        recentStreak: 3,
-        highestStreak: 3,
+      },
+    },
+    streaks: {
+      drink_water: {
+        highest: 6,
+        current: 4,
+      },
+      break_from_screen: {
+        highest: 8,
+        current: 8,
+      },
+      stretch: {
+        highest: 3,
+        current: 2,
+      },
+      task4: {
+        highest: 0,
+        current: 0,
+      },
+      task5: {
+        highest: 0,
+        current: 0,
       },
     },
   },
