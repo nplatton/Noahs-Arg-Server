b430a3bee7ced8f337785232828c745e6a077fcc
Noah Platton <noahplatton@gmail.com>
2022-02-21 17:49:45 +0000

    Set up index, create, destroy routes

--- api/server.js
+++ api/server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const server = express();
-server.use(cors());
+server.use(cors("*"));
 server.use(express.json());
 
-server.get('/', (req, res) => res.send('Welcome to Noahs-Arg-Server!'));
+server.get("/", (req, res) => res.send("Welcome to Noahs-Arg-Server!"));
 
-const usersRoutes = require('./routes/users');
-server.use('/users', usersRoutes);
+const usersRoutes = require("./routes/users");
+server.use("/users", usersRoutes);
 
 module.exports = server;
