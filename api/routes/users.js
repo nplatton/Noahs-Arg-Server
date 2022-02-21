ef256402d0ed1a1a63951b7b30c2a2746dd5bfaf
bojin11 <15011278723@163.com>
2022-02-21 15:24:42 +0000

    changes

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,8 +5,8 @@ const usersController = require('../controllers/users')
 router.get('/:org/users', usersController.index)
 router.get('/:org/users/:username', usersController.show)
 router.post('/:org/users/', usersController.create)
-router.patch('/:org/users/:username/habits/', habitsController.updateHabit)
-router.delete('/:org/users/:username/habits/:habit/', habitsController.destroyHabit)
+router.patch('/:org/users/:username/habits/', usersController.updateHabit)
+router.delete('/:org/users/:username/habits/:habit/', usersController.destroyHabit)
 
 
 module.exports = router;
