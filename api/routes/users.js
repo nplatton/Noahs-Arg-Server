b82779575a47133b546ac1461bc008db0e127575
Noah Platton <noahplatton@gmail.com>
2022-02-23 19:51:18 +0000

    Fix the route paths

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,7 +5,7 @@ const usersController = require("../controllers/users");
 const { verifyToken } = require("../middleware/auth");
 
 router.get("/org/:orgName", verifyToken, usersController.index);
-router.get("/:username", usersController.show);
+router.get("/:username", verifyToken, usersController.show);
 router.post("/", usersController.create);
 router.delete("/:username", verifyToken, usersController.destroyUser);
 router.get("/:username/habits/", verifyToken, usersController.getAllHabits);
@@ -15,6 +15,6 @@ router.patch(
   verifyToken,
   usersController.updateSingleHabit
 );
-router.patch("/:username/yes", usersController.clearHabits);
+router.delete("/:username/habits", verifyToken, usersController.clearHabits);
 
 module.exports = router;
