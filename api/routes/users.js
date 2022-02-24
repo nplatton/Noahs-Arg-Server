de4efdf341226ce3714a0439d34e538fa5777cd3
Noah Platton <noahplatton@gmail.com>
2022-02-24 08:15:06 +0000

    Final fix

--- api/routes/users.js
+++ api/routes/users.js
@@ -7,7 +7,7 @@ const { verifyToken } = require("../middleware/auth");
 router.get("/org/:orgName", verifyToken, usersController.index);
 router.get("/:username", verifyToken, usersController.show);
 router.post("/", usersController.create);
-router.delete("/:username", verifyToken, usersController.destroyUser);
+// router.delete("/:username", verifyToken, usersController.destroyUser);
 router.get("/:username/habits/", verifyToken, usersController.getAllHabits);
 router.patch("/:username/habits/", verifyToken, usersController.updateHabits);
 router.patch(
